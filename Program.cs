ae9744e1190f9f47c22d1750139c9237152ba6ca
Tatiana <vyatkinatanya98@gmail.com>
2022-09-19 00:25:59 +0500

    printing out the new array

--- Program.cs
+++ Program.cs
@@ -14,3 +14,12 @@ void Array2WithIF(string[] array1, string[] array2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
