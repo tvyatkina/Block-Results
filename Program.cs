4ba95e5c20b1ea30c2f41aa5186fad2553c31db4
Tatiana <vyatkinatanya98@gmail.com>
2022-09-19 00:55:08 +0500

    added drawio

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ void Array2WithIF(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-        if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
             array2[count] = array1[i];
             count++;
