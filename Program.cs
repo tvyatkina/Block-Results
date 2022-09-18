b6fe7b193358dd15268740ac90628093834e2c4e
Tatiana <vyatkinatanya98@gmail.com>
2022-09-19 00:25:12 +0500

    sorting out the initial array

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.WriteLine("Введите массив строк через пробел:");
 string? a = Console.ReadLine();
 string[] array1 = a!.Split(' ');
+
+void Array2WithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
