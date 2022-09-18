b0e0f5ff0e8a1826f56bae1d59946779e92c5658
Tatiana <vyatkinatanya98@gmail.com>
2022-09-19 00:27:22 +0500

    minor changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Введите массив строк через пробел:");
 string? a = Console.ReadLine();
 string[] array1 = a!.Split(' ');
+string[] array2 = new string[array1.Length];
 
 void Array2WithIF(string[] array1, string[] array2)
 {
@@ -23,3 +24,6 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+
+Array2WithIF(array1, array2);
+PrintArray(array2);
